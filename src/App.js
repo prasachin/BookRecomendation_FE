80766905af7e282702b9ee92bf36d3aafe13c727
sachin <sachinp@iitbhilai.ac.in>
2024-07-10 19:52:18 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BookList from "./components/Books/BookList";
+import Profile from "./components/UserProfile/UserProfile";
+import Recommendations from "./components/Books/Recommendation";
+import AuthCard from "./components/Auth/AuthCard";
+import BookDetails from "./components/Books/BookDetails";
+import "./style.css";
+import Header from "./components/Common/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route exact path="/login" element={<AuthCard />} />
+          <Route exact path="/register" element={<AuthCard />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/recommendations" element={<Recommendations />} />
+          <Route path="/books/:id" element={<BookDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
